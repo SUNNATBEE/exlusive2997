112c6c561bdce721cdaee78bb2a48f0edc7a59ed
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-12-04 18:55:40 +0300

    Yahyodan

--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import App from './App.jsx'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
-import Signup from './pages/registers/Signup'
 import Wishlist from './pages/Wishlist'
 import Cart from './pages/Cart'
+import SaginUp from "./pages/registers/SaginUp.jsx";
+
+import { Provider } from 'react-redux'
+import { store } from './redux/store/store'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
+// ROUTER
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-    children:[
-      {
-      path: "/about",
-    element: <About/>,
-    },
-    {
-      index:true,
-    element: <Home/>,
-    },
-    {
-      path: "/contact",
-    element: <Contact/>,
-    },
-    {
-      path: "/signup",
-    element: <Signup/>,
-    },
-    {
-      path: "/wishlist",
-    element: <Wishlist/>,
-    },
-    {
-      path: "/cart",
-    element: <Cart/>,
-    },
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "contact", element: <Contact /> },
+      { path: "signup", element: <SaginUp /> },
+      { path: "wishlist", element: <Wishlist /> },
+      { path: "cart", element: <Cart /> },
     ]
-  },
+  }
 ]);
 
+// RENDER
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}>
-    <App />
-  </RouterProvider>,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
