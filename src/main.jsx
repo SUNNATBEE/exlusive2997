e1489bbfeb37c10f6091b05f821012e11aa29046
noname306 <ya.sitora11@gmail.com>
2025-12-05 17:22:38 +0500

    SW

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,10 @@ import Contact from './pages/Contact'
 import Home from './pages/Home'
 import Wishlist from './pages/Wishlist'
 import Cart from './pages/Cart'
-<<<<<<< HEAD
-import Error from './pages/Error.jsx'
-=======
 import SaginUp from "./pages/registers/SaginUp.jsx";
 
 import { Provider } from 'react-redux'
 import { store } from './redux/store/store'
->>>>>>> main
 
 import {
   createBrowserRouter,
@@ -27,36 +23,6 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-<<<<<<< HEAD
-    errorElement: <Error />,    
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-      },
-      {
-        path: "wishlist",
-        element: <Wishlist />,
-      },
-      {
-        path: "cart",
-        element: <Cart />,
-      },
-    ]
-  }
-=======
     children: [
       { index: true, element: <Home /> },
       { path: "about", element: <About /> },
@@ -72,17 +38,11 @@ const router = createBrowserRouter([
         path: "*",
         element: <Error />
       }
->>>>>>> main
 ]);
 
 // RENDER
 createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
-  <RouterProvider router={router} />
-);
-=======
   <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>
 )
->>>>>>> main
